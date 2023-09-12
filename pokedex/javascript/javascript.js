5a930f4db576ea7465792491cd7e2db3971d9abd
RafaelValiati <131008596+RafaelValiati@users.noreply.github.com>
2023-09-12 16:33:19 -0300

    new

---
+++ pokedex/javascript/javascript.js
@@ -0,0 +1,32 @@
+const offset=0;
+const limit=10;
+const url ='https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}'
+
+function convertPokemonToLi(pokemon){
+    return '<li class="pokemon">' +
+    '<span class="number">#001</span>' +
+    '<span class="name">${pokemon.name}</span>' +
+    '<div class="detail">' +
+    '<ol class="types">' +
+    '<li class="type">grass</li>' +
+    '<li class="type">poison</li>' +
+    '</ol>' +
+    '<img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png" alt="${pokemon.name}">' +
+    '</div>' +
+    '</li>';
+    
+    
+
+}
+
+
+fetch(url)
+    .then((response) => response.json())
+    .then((jsonBody) => jsonBody.results)
+    .then((pokemonList) => {
+        for (let i = 0; i < pokemonList.length; i++) {
+            const pokemon = pokemonList[i];
+            console.log(convertPokemonToLi(pokemon))
+        }
+    })
+    .catch((error) => console.error(error))
